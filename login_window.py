cb7a327611dd888a81a0e707a8037da11ce9d2c5
LeoSuave25 <suaverdez90@gmail.com>
2023-07-19 13:36:43 +0800

    Records Window Opening Added if the Login is Successful

--- login_window.py
+++ login_window.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-
+from records_window import RecordsWindow
 class LoginWindow(tk.Toplevel):
     def __init__(self):
         super().__init__()
 
         self.title("Login")
         self.geometry("300x150")
+        self.configure(background="#d4d7fa")
 
         self.username_label = ttk.Label(self, text="Username", font=("Arial",10))
         self.username_label.place(relx=0.1, rely=0.1)
@@ -23,6 +24,8 @@ class LoginWindow(tk.Toplevel):
         self.login_button.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
 
+    def open_records_window(self):
+        Records = RecordsWindow()
     def login(self):
         # Implement your login logic here
         username = self.username_entry.get()
@@ -30,7 +33,8 @@ class LoginWindow(tk.Toplevel):
 
         # Check if the login is successful
         if username == "admin" and password == "password":
-            messagebox.showinfo("Success","Successfully Logged In")   
+            messagebox.showinfo("Success","Successfully Logged In")
+            self.open_records_window()      
             self.destroy()
         else:
             messagebox.showerror("Error","Wrong Username or Password")
